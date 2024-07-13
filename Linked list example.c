b3c0867b3f3074f25e03faa7e66774b406121a4e
Roland Labana <rolandlabana@gmail.com>
2024-07-12 21:18:29 -0500

    first commit

---
+++ Linked list example.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void insertAtBeginning(struct Node** head, int newData) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = (*head);
+    (*head) = newNode;
+}
+
+void insertAtEnd(struct Node** head, int newData) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* last = *head;
+    newNode->data = newData;
+    newNode->next = NULL;
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    while (last->next != NULL) {
+        last = last->next;
+    }
+    last->next = newNode;
+}
+
+void deleteNode(struct Node** head, int key) {
+    struct Node *temp = *head, *prev;
+    if (temp != NULL && temp->data == key) {
+        *head = temp->next;
+        free(temp);
+        return;
+    }
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+    if (temp == NULL) return;
+    prev->next = temp->next;
+    free(temp);
+}
+
+void displayList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d -> ", node->data);
+        node = node->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    printf("Welcome to Linked List Example in C\n");
+    insertAtEnd(&head, 1);
+    insertAtEnd(&head, 2);
+    insertAtBeginning(&head, 0);
+    displayList(head);
+    deleteNode(&head, 2);
+    displayList(head);
+    return 0;
+}
