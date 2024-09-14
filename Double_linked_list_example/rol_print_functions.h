ee3435d5ec36a0976779d9b872726570999d19cc
Roland Labana <rolandlabana@gmail.com>
2024-09-14 10:56:13 -0700

    added fwd struct def in print functions header file to cure build error

--- Double_linked_list_example/rol_print_functions.h
+++ Double_linked_list_example/rol_print_functions.h
@@ -3,8 +3,13 @@
 #ifndef ROL_PRINT_FUNCTIONS_H
 #define ROL_PRINT_FUNCTIONS_H
 
+struct PersonNode;  // this is a forward def of the struct to indicate it's defined elsewhere
+                    // omitting this will give an error during build
+
 void print_hello_world(int numTimes);
 void printListDiagramVerticalPadding(struct PersonNode* head, struct PersonNode* tail);
 
 #endif // ROL_PRINT_FUNCTIONS_H
  
+
+
